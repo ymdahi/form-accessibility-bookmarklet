0a3927f26d1b559c103abd4a8d5edda1bc8bb12d
wlu_ydahi <ydahi@wlu.ca>
2018-12-03 15:02:44 -0500

    enter jquery

--- accessible-canli.js
+++ accessible-canli.js
@@ -1,14 +1,41 @@
 (function () {
 
+    // the minimum version of jQuery we want
+    var v = "1.3.2";
+
+    // check prior inclusion and version
+    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+      var done = false;
+      var script = document.createElement("script");
+      script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+      script.onload = script.onreadystatechange = function(){
+        if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+          done = true;
+          initMyBookmarklet();
+        }
+      };
+      document.getElementsByTagName("head")[0].appendChild(script);
+    } else {
+      initMyBookmarklet();
+    }
+
+    function initMyBookmarklet() {
+      (window.myBookmarklet = function() {
+        var test = $('.termText input');
+        console.log(test);
+        
+      })();
+    }
+
     // var form = document.getElementsByTagName("input[type='text']");
     // console.log(form);
     
     // document text input
     //var documentTextInput = document.getElementsByClassName("termText").getElementsByTagName("input");
 
-    var labelElement = "<label for='textInput'>Document text</label>";
-    var parent = document.querySelectorAll("div.termText");
-    parent.appendChild(labelElement);
+    // var labelElement = "<label for='textInput'>Document text</label>";
+    // var parent = document.querySelectorAll("div.termText");
+    // parent.appendChild(labelElement);
 
     
     
