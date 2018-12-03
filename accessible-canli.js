8db24822983ef11af4a8c2d3b8a6229c6d7bb73e
wlu_ydahi <ydahi@wlu.ca>
2018-12-03 14:44:59 -0500

    get document text input

--- accessible-canli.js
+++ accessible-canli.js
@@ -4,7 +4,8 @@
     // console.log(form);
     
     // document text input
-    var documentTextInput = document.getElementsByClassName("termText").getElementsByTagName("input");
+    //var documentTextInput = document.getElementsByClassName("termText").getElementsByTagName("input");
+    var documentTextInput = document.querySelectorAll(".termText > input");
     var documentTextPlaceholder = documentTextInput.placeholder;
     console.log(documentTextInput);
     console.log('placeholder: ' + documentTextPlaceholder);
